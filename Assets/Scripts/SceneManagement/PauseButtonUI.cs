aa43e0973986d52990b65c67dd015a356d883484
YoopMoon <obipan.2024@hotmail.com>
2026-04-16 12:35:56 +0200

    Add pause menu UI, persistent coin counte and improved HUD feedback

---
+++ Assets/Scripts/SceneManagement/PauseButtonUI.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    [Header("Pause Elements")]
+    [SerializeField] private GameObject pauseButton;
+    [SerializeField] private GameObject pauseScreen;
+    [SerializeField] private GameObject menuPause;
+
+    public void OpenPauseMenu()
+    {
+        if (pauseButton != null)
+            pauseButton.SetActive(false);
+
+        if (pauseScreen != null)
+            pauseScreen.SetActive(true);
+
+        if (menuPause != null)
+            menuPause.SetActive(true);
+
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        if (pauseButton != null)
+            pauseButton.SetActive(true);
+
+        if (pauseScreen != null)
+            pauseScreen.SetActive(false);
+
+        if (menuPause != null)
+            menuPause.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Abrir ajustes");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Ir a Main Menu");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("CharacterSelectScene");
+    }
+}
